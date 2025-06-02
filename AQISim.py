d0e03deb73fc6dc3ac8b0f506615603f38ead43e
OJOetw <evd12147@gamil.com>
2025-06-02 14:52:57 +0900

    almost complete

--- AQISim.py
+++ AQISim.py
@@ -1,6 +1,6 @@
 wind_speed_input = int(input("Enter the wind speed(km/h): "))
 factory_emission_level_input = int(input("Enter the factory emmision level(0~10): "))
-traffic_volume_input = input("Enter traffic volume: ").lower()
+traffic_volume_input = input("Enter traffic volume(high or medium): ").lower()
 raining_input = input("Is it raining?(y/n):　").lower() == "y"
 number_of_simulation_days_input = int(input("number of simulation days: "))
 
@@ -13,7 +13,8 @@ def simulate_pm25(wind_speed_input, factory_emission_level_input,
 
     for day in range(1, number_of_simulation_days_input + 1):
         change = 0
-
+        #change是pm25的變化量
+        
         if wind_speed_input > 15:
             change -= 10
         elif wind_speed_input < 5:
@@ -35,6 +36,8 @@ def simulate_pm25(wind_speed_input, factory_emission_level_input,
 
 
         pm25 = max(0, pm25 + change)
+              #確保低於0時，會顯示0不會顯示負數
+        
         record.append((day, pm25))
 
     return record
@@ -47,10 +50,6 @@ result = simulate_pm25(wind_speed_input,
 print("simulation result: ")
 
 for day, value in result:
+    #result(day,pm25)
     print(f"Day {day}: PM2.5 = {value} μg/m³")
-
-
-
-
-
 
